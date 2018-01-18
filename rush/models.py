226f0221c2ae84477ac9c8943f8463f2398d409e
vkhemlani96 <vkhemlani96@gmail.com>
2018-01-17 19:34:25 -0500

    Created RushProfile and RushApplication models

--- rush/models.py
+++ rush/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.postgres import fields
 
 class RushEventLocation(models.Model):
 	name = models.CharField(max_length=50)
@@ -68,3 +69,37 @@ class RushEvent(models.Model):
 			location += " (" + self.room_long_name + ")"
 
 		return location
+
+class RushProfile(models.Model):
+	FRESHMAN = "FR"
+	SOPHOMORE = "SO"
+	JUNIOR = "JR"
+	SENIOR = "SR"
+
+	first_name = models.CharField(max_length=20)
+	last_name = models.CharField(max_length=20)
+	email = models.CharField(max_length=8, blank=True, null=True)
+	semester = models.CharField(max_length=3)
+	phone_number = models.CharField(max_length=11)
+	major_schools = fields.ArrayField(models.CharField(max_length=3)) # max_length => CAS,ENG,QST
+	grade = models.CharField(
+		max_length=2, choices=((FRESHMAN, "Freshman"), (SOPHOMORE, "Sophomore"), (JUNIOR, "Junior"), (SENIOR, "Senior")))
+	channel = models.CharField(max_length=30)
+	events_attended = fields.ArrayField(models.CharField(max_length=50))
+	submitted_application = models.BooleanField()
+	interview_wave = models.PositiveSmallIntegerField()
+	interview_prelim_yes = models.PositiveSmallIntegerField()
+	interview_prelim_no = models.PositiveSmallIntegerField()
+	interview_prelim_abstain = models.PositiveSmallIntegerField()
+	interview_prelim_deliberate = models.BooleanField()
+	interview_final_yes = models.PositiveSmallIntegerField()
+	interview_final_no = models.PositiveSmallIntegerField()
+	interview_final_abstain = models.PositiveSmallIntegerField()
+	given_bid = models.BooleanField()
+
+class RushApplication(models.Model):
+	profile = models.OneToOneField(RushProfile, on_delete=models.CASCADE, related_name='application')
+	timestamp = models.DateTimeField(auto_now_add=True)
+	address = models.CharField(max_length=15)
+	gpa = models.CharField(max_length=5)
+	application_answers = fields.ArrayField(models.TextField())
