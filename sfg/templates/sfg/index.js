4e1ce9641aa4d10d19da4e707d4f7a6192abab49
James Bishop <jtbishop@bu.edu>
2020-08-01 14:31:12 -0400

    img fixing

--- sfg/templates/sfg/index.js
+++ sfg/templates/sfg/index.js
@@ -1,45 +1,18 @@
-$("#rush_theme h2 a.stand_out").each(function(index) {
-	var anchor = $(this);
-	setTimeout(function() {
-		console.log(anchor.css("color"));
-		anchor.animate({
-			color: "white"
-		}, 600);
-	}, index * 1000)
-});
-setTimeout(function() {
-	$("#rush_theme > div").css("display", "block");
-	$("#rush_theme > div").animate({
-		opacity: 1
-	}, 700);
-}, 3200)
-
 //Counter to go throw array containing image links
 var imgCellIndex = 1;
 //Array of arrays that contain imgs to display in each cell
 var imgCell = [ [
 "citigroup",
-"ey",
-"fidelity",
-"kpmg",
-"boomberg",
+"kbcm",
 ],[
-"pwc",
-"fleishmanhilliard",
-"ritzcarlton",
 "ubs",
-"deloitte",
 ],[
-"ibm",
-"boa",
-"jpmorgan",
-"hsbc",
-"accenture",
+"santander"
 ]];
 
 function changeImgCell(index) {
 	$(".imgCell" + index).fadeOut(function() {
-		$(this).attr("src", "/static/buakpsi/img/companies/" + imgCell[index][imgCellIndex] + "_cropped.png");
+		$(this).attr("src", "/static/sfg/img/placements/" + imgCell[index][imgCellIndex] + "_cropped.png");
 	}).fadeIn();
 	if (index == 0) {
 		imgCellIndex = (imgCellIndex + 1) % 5;	
